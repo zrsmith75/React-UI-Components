05b6d817bdaaba0985f7e7a7009750e28d846a1a
Zac Smith <zrs3141592@gmail.com>
2019-05-29 14:45:20 -0600

    finish card MVP

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -2,7 +2,13 @@ import React from 'react';
 import './Card.css';
 
 const CardContent = () => {
-  return <p>STuff</p>
+  return (
+    <div className='content'>
+      <p className='contentTitle'>Get Started with React</p>
+      <p>React makes it painless to create interactice UIs. Design simple views for each state in your application.</p>
+      <a href="#">reactjs.org</a>
+    </div>
+  );
 };
 
 
